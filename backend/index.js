a06b560429fe70e9d5cc5ab9bc283213cf07c185
Savze7 <didaattalah@gmail.com>
2026-05-13 02:03:42 +0700

    new index

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+
+const noteRoutes = require("./routes/noteRoutes");
+
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+// Middleware
+app.use(cors());
+app.use(express.json());
 
+// Route test
 app.get("/", (req, res) => {
-  res.send("OK HIDUP");
+  res.send("Backend API is running 🚀");
 });
 
+// Routes utama
+app.use("/api/v1/notes", noteRoutes);
+
+// 🚀 WAJIB Cloud Run
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("SERVER HIDUP DI " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
